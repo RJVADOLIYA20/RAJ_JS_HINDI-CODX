4f78cd50a50f5153f5ee2485532f10ba76bae4ee
RJVADOLIYA20 <134076675+RJVADOLIYA20@users.noreply.github.com>
2024-03-16 11:01:44 +0000

    devcontainer changes done

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1444553;
+let accountEmail = "rajV1@gamil.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2  //not Allowed
+ 
+accountEmail = "hc@hc.com"
+accountPassword="2121212"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
